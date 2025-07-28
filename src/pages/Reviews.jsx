d1a0819426d33593123f8628e60ff60f21840ea5
Mvinit2245 <vinitmorankar22@gmail.com>
2025-07-28 23:04:01 +0530

    second commit

--- src/pages/Reviews.jsx
+++ src/pages/Reviews.jsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from "react";
 import axios from "axios";
-import axios from "../api/axios";
+
 
 axios.get("/reviews")  // auto-appends to baseURL
 axios.post("/reviews", newReview)
